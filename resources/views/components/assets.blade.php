c98ed8d961c41f825f914e7a7f51fdc1ee6e7edc
Schwarzer Tenshi <schwarzertenshi90@gmail.com>
2026-06-24 20:47:35 +0700

    Update

--- resources/views/components/assets.blade.php
+++ resources/views/components/assets.blade.php
@@ -24,3 +24,5 @@
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
